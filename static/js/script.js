0c5518b56cb0ca0a85fa4f3654ea64a728dacf21
MarcJPH <marchuntzinger@hotmail.com>
2021-08-20 19:48:07 +0000

    Corrected issues found in HTML and CSS checks

--- static/js/script.js
+++ static/js/script.js
@@ -9,14 +9,3 @@ $(document).ready(function () {
         $('select').formSelect();
     });
 });
-
-function confirm_delete() {
-    var confirmation = confirm("Are you sure to want to delete this activity?");
-    if (confirmation) {
-        window.location.href = "{{ url_for('delete_activity', activity_id=places._id) }}";
-        alert("The activity has been deleted");
-    }
-    else {
-        alert("The activity was not deleted");
-    }
-}
